272cb9af5bc2599f66ab09a38db6115311944208
Manzi Kevin <manzikevin04@gmail.com>
2026-05-18 22:14:10 +0200

    My first Object

---
+++ src/Car.java
@@ -0,0 +1,14 @@
+
+public class Car {
+    String make = "BMW";
+    String model = "M3";
+    double price = 1000.00;
+
+    void drive(){
+        IO.println("You Driving The car");
+    }
+
+    void brake(){
+        IO.println("You step on Brakes");
+    }
+}
